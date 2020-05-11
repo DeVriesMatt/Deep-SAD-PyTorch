3e83d89e5668940b3edfbb01b2bf9fbbe838490e
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-11 13:42:22 +0100

    can't load mode scenario 5

--- src/run_deepSAD_5.sh
+++ src/run_deepSAD_5.sh
@@ -13,8 +13,6 @@ do
 	    for dims in 16 32 64 256 512
 	    do
 			CUDA_VISIBLE_DEVICES=1 python main.py fmnist fmnist_LeNet ../log/fmnist/scenario_5/deepSAD/ ../data  \
-			--load_model ../log/fmnist/scenario_1/deepSAD/model_0_1_1_0.tar \
-      --load_ae_only True \
 			--ratio_known_outlier 0.05 --ratio_pollution 0.1 \
 			--lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 150 \
 			--ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class $normal_class --known_outlier_class $unknown_class --eta 1. \
