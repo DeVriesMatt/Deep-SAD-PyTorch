985eeef6eb2c4b732958f748d001365e97ba3a03
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-11 01:41:16 +0100

    pretrain

--- src/run_deepSAD_4.sh
+++ src/run_deepSAD_4.sh
@@ -15,7 +15,7 @@ do
 		do
 
 			CUDA_VISIBLE_DEVICES=1 python main.py fmnist fmnist_LeNet ../log/fmnist/scenario_4/deepSAD/ ../data \
-			--load_model ../../model_0_1_1_0.tar \
+			--load_model ../log/fmnist/scenario_1/deepSAD/model_0_1_1_0.tar \
       --load_ae_only True\
 			--ratio_known_outlier 0.05 --ratio_pollution 0.1 \
 			--lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 150 \
