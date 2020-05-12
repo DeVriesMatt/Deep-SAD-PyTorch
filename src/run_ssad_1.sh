9e11b11c7a0fb3a3ea1e3b0df59e3608f18e33af
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-12 17:38:38 +0100

    updat ssad

--- src/run_ssad_1.sh
+++ src/run_ssad_1.sh
@@ -13,9 +13,9 @@ do
 		for gamma_l in 0.01 0.05 0.1 0.2
 		do
 			CUDA_VISIBLE_DEVICES=1 python baseline_ssad.py fmnist ../log/fmnist/scenario_1/ssad/ ../data --ratio_known_outlier $gamma_l --ratio_pollution 0.1 \
-		  --kernel rbf --kappa 1.0 --hybrid False --load_ae ../log/DeepSAD/fashion_mnist_test/model.tar --batch_size 200 --normal_class $normal_class \
+		  --kernel rbf --kappa 1.0 --hybrid False --load_ae ../log/DeepSAD/fashion_mnist_test/model.tar --normal_class $normal_class \
 		  --known_outlier_class $unknown_class \
-			--n_known_outlier_classes 1 --seed 0 --n_jobs_dataloader 6 
+			--n_known_outlier_classes 1 --seed 0 --n_jobs_dataloader 6
 
 		done
 	done
