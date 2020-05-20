99fc958c61256986059a6f01abb0821dda9928c3
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-20 23:53:00 +0100

    ssad raw

--- src/run_ssad_3.sh
+++ src/run_ssad_3.sh
@@ -21,19 +21,19 @@ do
 				--case 3 \
 				--n_jobs_dataloader 8;
 
-			python baseline_ssad.py cifar10 ../log/fmnist/scenario_3/ssad_hybrid ../data \
-				--ratio_known_outlier 0.05 \
-				--ratio_pollution 0.1 \
-				--kernel rbf \
-				--kappa 1.0 \
-				--hybrid True \
-				--load_ae ../log/cifar-10/scenario_2/deepSAD/model_${normal_class}_${unknown_class}_005.tar \
-				--normal_class $normal_class \
-				--known_outlier_class 0 \
-				--n_known_outlier_classes $kappa \
-				--seed $seed \
-				--case 3 \
-				--n_jobs_dataloader 8;
+#			python baseline_ssad.py fmnist ../log/fmnist/scenario_3/ssad_hybrid ../data \
+#				--ratio_known_outlier 0.05 \
+#				--ratio_pollution 0.1 \
+#				--kernel rbf \
+#				--kappa 1.0 \
+#				--hybrid True \
+#				--load_ae ../log/fmnist/scenario_2/deepSAD/model_${normal_class}_${unknown_class}_005.tar \
+#				--normal_class $normal_class \
+#				--known_outlier_class 0 \
+#				--n_known_outlier_classes $kappa \
+#				--seed $seed \
+#				--case 3 \
+#				--n_jobs_dataloader 8;
 		done
 	done
 done
