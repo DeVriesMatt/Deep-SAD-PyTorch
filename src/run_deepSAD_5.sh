860003b28466cde660e73ca53c1097f011388468
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-20 19:36:35 +0100

    deep sad 5

--- src/run_deepSAD_5.sh
+++ src/run_deepSAD_5.sh
@@ -3,7 +3,7 @@
 ################################3
 # Scenario 1
 # This is the deepSAD Configuration
-for normal_class in 0 1 2 3 4 5 6 7 8 9
+for normal_class in 2 3 4 5 6 7 8 9
 do
 	for unknown_class in 0 1 2 3 4 5 6 7 8 9
 	do
@@ -13,6 +13,8 @@ do
 	    for dims in 16 32 64 256 512
 	    do
 			CUDA_VISIBLE_DEVICES=1 python main.py fmnist fmnist_LeNet ../log/fmnist/scenario_5/deepSAD/ ../data  \
+			--load_model ../log/fmnist/scenario_1/deepSAD/model_0_1_1_0.tar \
+      --load_ae_only True\
 			--ratio_known_outlier 0.05 --ratio_pollution 0.1 \
 			--lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 --pretrain True --ae_lr 0.0001 --ae_n_epochs 150 \
 			--ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class $normal_class --known_outlier_class $unknown_class --eta 1. \
