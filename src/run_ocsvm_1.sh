931d434bd5411a8209140f14eb6ba75e24f16e39
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-24 21:16:47 +0100

    ocsvm hybrid

--- src/run_ocsvm_1.sh
+++ src/run_ocsvm_1.sh
@@ -13,16 +13,16 @@ do
 		for gamma_l in 0.01 0.05 0.1 0.2
 		do
 			# OC-SVM
-			python baseline_ocsvm.py fmnist ../log/fmnist/scenario_1/ocsvm ../data \
-				--ratio_known_outlier $gamma_l \
-				--ratio_pollution 0.1 \
-				--kernel rbf \
-				--normal_class $normal_class \
-				--known_outlier_class $unknown_class \
-				--n_known_outlier_classes 1 \
-				--seed 0 \
-        --case 1 \
-				--n_jobs_dataloader 8;
+#			python baseline_ocsvm.py fmnist ../log/fmnist/scenario_1/ocsvm ../data \
+#				--ratio_known_outlier $gamma_l \
+#				--ratio_pollution 0.1 \
+#				--kernel rbf \
+#				--normal_class $normal_class \
+#				--known_outlier_class $unknown_class \
+#				--n_known_outlier_classes 1 \
+#				--seed 0 \
+#        --case 1 \
+#				--n_jobs_dataloader 8;
 
 	# 		# OC-SVM Hybrid
 			python baseline_ocsvm.py fmnist ../log/fmnist/scenario_1/ocsvmHybrid ../data \
@@ -36,7 +36,8 @@ do
 			--hybrid True \
 			--load_ae ../log/fmnist/scenario_1/deepSAD/model_${normal_class}_${unknown_class}_0.tar \
       --case 1 \
-			--n_jobs_dataloader 8
+			--n_jobs_dataloader 8 \
+			--feat_dims 128
 		done
 	done
 done
