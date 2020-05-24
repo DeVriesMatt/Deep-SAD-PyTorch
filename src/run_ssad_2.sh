4cfe17531a032c7667f72c9adc6086ec371984a5
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-24 22:10:17 +0100

    ssad 2 hybrid

--- src/run_ssad_2.sh
+++ src/run_ssad_2.sh
@@ -9,20 +9,20 @@ do
 	do
 		if [ $normal_class -eq $unknown_class ]; then
       		continue
-	    fi
+	  fi
 		for gamma_p in 0. 0.01 0.05 0.1 0.2
 		do
-			 python baseline_ssad.py fmnist ../log/fmnist/scenario_2/ssad_raw ../data \
-			 	--ratio_known_outlier 0.05 \
-			 	--ratio_pollution $gamma_p \
-			 	--kernel rbf \
-			 	--kappa 1.0 \
-			 	--normal_class $normal_class \
-			 	--known_outlier_class $unknown_class \
-			 	--n_known_outlier_classes 1 \
-			 	--seed 0 \
-			 	--case 2 \
-			 	--n_jobs_dataloader 8;
+#			 python baseline_ssad.py fmnist ../log/fmnist/scenario_2/ssad_raw ../data \
+#			 	--ratio_known_outlier 0.05 \
+#			 	--ratio_pollution $gamma_p \
+#			 	--kernel rbf \
+#			 	--kappa 1.0 \
+#			 	--normal_class $normal_class \
+#			 	--known_outlier_class $unknown_class \
+#			 	--n_known_outlier_classes 1 \
+#			 	--seed 0 \
+#			 	--case 2 \
+#			 	--n_jobs_dataloader 8;
 
 			python baseline_ssad.py fmnist ../log/fmnist/scenario_2/ssad_hybrid ../data \
 				--ratio_known_outlier 0.05 \
@@ -30,7 +30,7 @@ do
 				--kernel rbf \
 				--kappa 1.0 \
 				--hybrid True \
-				--load_ae ../log/fmnist/scenario_2/deepSAD/model_${normal_class}_${unknown_class}_005.tar \
+				--load_ae ../log/fmnist/scenario_2/deepSAD/model_0_${unknown_class}_005.tar \
 				--normal_class $normal_class \
 				--known_outlier_class $unknown_class \
 				--n_known_outlier_classes 1 \
