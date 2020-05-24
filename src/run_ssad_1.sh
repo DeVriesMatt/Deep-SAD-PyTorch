7e49b7b6cb2c34c205f75ca80b3f6047aee53bb7
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-24 21:34:02 +0100

    ssad 1 hybrid

--- src/run_ssad_1.sh
+++ src/run_ssad_1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ################################3
-# Scenario 2
+# Scenario 1
 # This is the deepSAD Configuration
 for normal_class in 0 1 2 3 4 5 6 7 8 9
 do
@@ -12,17 +12,17 @@ do
 	    fi
 		for gamma_l in 0.01 0.05 0.1 0.2
 		do
-			python baseline_ssad.py fmnist ../log/fmnist/scenario_1/ssad_raw ../data \
-				--ratio_known_outlier $gamma_l \
-				--ratio_pollution 0.1 \
-				--kernel rbf \
-				--kappa 1.0 \
-				--normal_class $normal_class \
-				--known_outlier_class $unknown_class \
-				--n_known_outlier_classes 1 \
-				--seed 0 \
-				--case 1 \
-				--n_jobs_dataloader 8;
+#			python baseline_ssad.py fmnist ../log/fmnist/scenario_1/ssad_raw ../data \
+#				--ratio_known_outlier $gamma_l \
+#				--ratio_pollution 0.1 \
+#				--kernel rbf \
+#				--kappa 1.0 \
+#				--normal_class $normal_class \
+#				--known_outlier_class $unknown_class \
+#				--n_known_outlier_classes 1 \
+#				--seed 0 \
+#				--case 1 \
+#				--n_jobs_dataloader 8;
 
 			python baseline_ssad.py fmnist ../log/fmnist/scenario_1/ssad_hybrid ../data \
 				--ratio_known_outlier $gamma_l \
@@ -30,7 +30,7 @@ do
 				--kernel rbf \
 				--kappa 1.0 \
 				--hybrid True \
-				--load_ae ../log/fmnist/scenario_2/deepSAD/model_${normal_class}_${unknown_class}_005.tar \
+				--load_ae ../log/fmnist/scenario_1/deepSAD/model_${normal_class}_${unknown_class}_0.tar \
 				--normal_class $normal_class \
 				--known_outlier_class $unknown_class \
 				--n_known_outlier_classes 1 \
