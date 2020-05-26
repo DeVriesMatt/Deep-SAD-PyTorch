c602a9995e4e82b07a1fc120513906e54dc88494
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-27 00:40:37 +0100

    ssad hybrid test

--- src/run_ssad_1_hybrid.sh
+++ src/run_ssad_1_hybrid.sh
@@ -3,13 +3,13 @@
 ################################3
 # Scenario 1
 # This is the deepSAD Configuration
-for normal_class in 4 5 6 7 8 9
-do
-	for unknown_class in 0 1 2 3 4 5 6 7 8 9
-	do
-		if [ $normal_class -eq $unknown_class ]; then
-      		continue
-	  fi
+#for normal_class in 4 5 6 7 8 9
+#do
+#	for unknown_class in 0 1 2 3 4 5 6 7 8 9
+#	do
+#		if [ $normal_class -eq $unknown_class ]; then
+#      		continue
+#	  fi
 #		for gamma_l in 0.01 0.05 0.1 0.2
 #		do
 			python baseline_ssad.py fmnist ../log/fmnist/scenario_1/ssad_hybrid ../data \
@@ -18,13 +18,13 @@ do
 				--kernel rbf \
 				--kappa 1.0 \
 				--hybrid True \
-				--load_ae ../log/fmnist/scenario_1/deepSAD/model_${normal_class}_${unknown_class}_0.tar \
-				--normal_class $normal_class \
-				--known_outlier_class $unknown_class \
+				--load_ae ../log/fmnist/scenario_1/deepSAD/model_4_0_0.tar \
+				--normal_class 4 \
+				--known_outlier_class 0 \
 				--n_known_outlier_classes 1 \
 				--seed 0 \
 				--case 1 \
 				--n_jobs_dataloader 6
 #		done
-	done
-done
+#	done
+#done
