39ab596d92666b6abac8ec4dbab481a2b9ae3f2e
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-27 14:00:06 +0100

    ocsvm raw

--- src/run_ocSvm_2.sh
+++ src/run_ocSvm_2.sh
@@ -3,7 +3,7 @@
 ################################3
 # Scenario 2
 # This is the deepSAD Configuration
-for normal_class in 0 1 2 3 4 5 6 7 8 9
+for normal_class in 3 4 5 6 7 8 9
 do
 	for unknown_class in 0 1 2 3 4 5 6 7 8 9
 	do
@@ -13,31 +13,31 @@ do
 		for gamma_p in 0. 0.01 0.05 0.1 0.2
 		do
 #		  OC-SVM
-#			python baseline_ocsvm.py fmnist ../log/fmnist/scenario_2/ocsvm ../data \
-#			--ratio_known_outlier 0.05 \
-#			--ratio_pollution $gamma_p \
-#			--kernel rbf \
-#			--normal_class $normal_class \
-#			--known_outlier_class $unknown_class \
-#			--n_known_outlier_classes 1 \
-#			--seed 0 \
-#      --case 2 \
-#			--n_jobs_dataloader 8;
-
-	# 		# OC-SVM Hybrid
-	# 		# TODO Train the Deep-SAD with pretrain first and then load the pretrain weights to play with it!
-			python baseline_ocsvm.py fmnist ../log/fmnist/scenario_2/ocsvmHybrid ../data \
+			python baseline_ocsvm.py fmnist ../log/fmnist/scenario_2/ocsvm ../data \
 			--ratio_known_outlier 0.05 \
 			--ratio_pollution $gamma_p \
 			--kernel rbf \
 			--normal_class $normal_class \
 			--known_outlier_class $unknown_class \
 			--n_known_outlier_classes 1 \
-      --seed 0 \
-			--hybrid True \
-			--load_ae ../log/fmnist/scenario_2/deepSAD/model_0_${unknown_class}_005.tar \
+			--seed 0 \
       --case 2 \
-			--n_jobs_dataloader 8
+			--n_jobs_dataloader 8;
+
+	# 		# OC-SVM Hybrid
+#	# 		# TODO Train the Deep-SAD with pretrain first and then load the pretrain weights to play with it!
+#			python baseline_ocsvm.py fmnist ../log/fmnist/scenario_2/ocsvmHybrid ../data \
+#			--ratio_known_outlier 0.05 \
+#			--ratio_pollution $gamma_p \
+#			--kernel rbf \
+#			--normal_class $normal_class \
+#			--known_outlier_class $unknown_class \
+#			--n_known_outlier_classes 1 \
+#      --seed 0 \
+#			--hybrid True \
+#			--load_ae ../log/fmnist/scenario_2/deepSAD/model_0_${unknown_class}_005.tar \
+#      --case 2 \
+#			--n_jobs_dataloader 8
 		done
 	done
 done
