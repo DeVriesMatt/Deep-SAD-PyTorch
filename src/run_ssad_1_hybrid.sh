681760e8483c09dfa5a635fcc12fe079727c5f78
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-05-27 13:02:53 +0100

    deep SAD 2

--- src/run_ssad_1_hybrid.sh
+++ src/run_ssad_1_hybrid.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-################################3
+################################
 # Scenario 1
 # This is the deepSAD Configuration
-#for normal_class in 4 5 6 7 8 9
-#do
-#	for unknown_class in 0 1 2 3 4 5 6 7 8 9
-#	do
-#		if [ $normal_class -eq $unknown_class ]; then
-#      		continue
-#	  fi
-#		for gamma_l in 0.01 0.05 0.1 0.2
-#		do
+for normal_class in 4 5 6 7 8 9
+do
+	for unknown_class in 0 1 2 3 4 5 6 7 8 9
+	do
+		if [ $normal_class -eq $unknown_class ]; then
+      		continue
+	  fi
+		for gamma_l in 0.01 0.05 0.1 0.2
+		do
 			python baseline_ssad.py fmnist ../log/fmnist/scenario_1/ssad_hybrid ../data \
 				--ratio_known_outlier 0.01 \
 				--ratio_pollution 0.1 \
@@ -24,7 +24,7 @@
 				--n_known_outlier_classes 1 \
 				--seed 0 \
 				--case 1 \
-				--n_jobs_dataloader 6
-#		done
-#	done
-#done
+				--n_jobs_dataloader 6;
+		done
+	done
+done
